49482eb7a836f84e4511ad6805725da76ac74ca6
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2019-09-18 10:36:54 +0200

    t3

---
+++ ClassLibrary/MC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketLibrary
+{
+    class MC
+    {
+
+        public string Licensplate;
+        public DateTime Date;
+        public decimal Price(decimal price = 125)
+        {
+
+            return price;
+        }
+        public string VehicleType(string type = "MC")
+        {
+            return type;
+        }
+    }
+}
